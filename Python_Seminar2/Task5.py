d6caa7151b92ef8fb728cb00bfceaa84535cb97b
jay_sk <jay_sk@ngs.ru>
2022-12-24 01:59:44 +0700

    In task5 removed unnecessary comments.

--- Python_Seminar2/Task5.py
+++ Python_Seminar2/Task5.py
@@ -16,18 +16,3 @@ for i in range(1, number + 1):
 print(f'\nПервоначальный список:\n{list}')
 random.shuffle(list)
 print(f'\nИзменённый список:\n{list}')
-# print(f'\nСписок из N={number} элементов, заполненных числами из промежутка [-{number}, {number}]:\n{list}')
-
-# listsecond = []
-# for j in range(0, number):
-#     listsecond.append(random.randint(0, number))
-
-# listresult = []
-# for i in list:
-#     if i in listsecond:
-#         listresult.append(int(i))
-
-# print(f'\n{listsecond}, \n{listresult}')
-
-# print(f'\nСписок позиций для произведения элементов:\n{listsecond}')
-# print(f'\n{}')
